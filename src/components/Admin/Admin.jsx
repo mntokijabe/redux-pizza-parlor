13bcda431790d3dc53ad73f9fbd536a73eec399a
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-08-16 10:15:04 -0500

    admin list

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,46 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import '../App/App.css'
+
+function Admin() {
+    let [adminList, setAdminList] = useState([]);
+
+    axios ({
+        method: `GET`,
+        url: '/api/order'
+    }) .then ((response) => {
+        setAdminList(response.data);
+    }) .catch((error) => {
+        console.log(error);
+    });
+
+    
+    return (
+        <div>
+            <h2>Prime Pizza Orders</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Time Order Placed</th>
+                        <th>Type</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+                <tbody>{adminList.map(orders => {
+                return (
+                    <tr>
+                        <td>{orders.customer_name}</td>
+                        <td>{orders.time}</td>
+                        <td>{orders.type}</td>
+                        <td>{orders.cost}</td>
+                    </tr>
+                    )
+                })}</tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Admin;
